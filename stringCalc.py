e48ae8b662f402c356265059703bb2c779fe98cb
JP5252 <justin.pearson52@hotmail.com>
2023-02-03 09:28:02 -0700

    fix test 4 var's and pass test 4

--- stringCalc.py
+++ stringCalc.py
@@ -8,6 +8,10 @@ def add(numbers):
 
 	#if string has multiple numbers add them
 	elif ("," in numbers):
+		#split numbers into list deliminated by newline
+		lineList = numbers.split("\n")
+		#put lines together deliminated by commas
+		numbers = ",".join(lineList)
 		#split numbers into list deliminated by commas
 		numList = numbers.split(",")
 		sum = 0
@@ -35,11 +39,18 @@ def main():
 	elif (add(num2) == 1):
 		print("Test 1: Passed")
 	
-	#test 3 take in string with multiple numbers return added
+	#test 3 take in string with multiple numbers split by commas return added
 	num3 = "1,2"
 	if (add(num3) != 3):
 		print("Test 1: Failed")
 	elif (add(num3) == 3):
+		print("Test 1: Passed")
+
+	#test 4 take in string with multiple numbers split by newline return added
+	num4 = "1,2\n3"
+	if (add(num4) != 6):
+		print("Test 1: Failed")
+	elif (add(num4) == 6):
 		print("Test 1: Passed")
 
 if __name__ == '__main__':
