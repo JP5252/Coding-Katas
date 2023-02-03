c082e367a114bb7b4fdf7bdd5e12519adf5dedf4
JP5252 <justin.pearson52@hotmail.com>
2023-02-02 23:11:09 -0700

    pass test 1

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,13 +1,13 @@
 def fizzBuzz(num):
-
+	num = str(num)
 	return num
 
 def main():
 	num1 = 10
 	if (type(fizzBuzz(num1)) == int):
-		print("Test1 Failed")
+		print("Test 1: Failed")
 	elif (type(fizzBuzz(num1)) == str):
-		print("Test1 Passed")
+		print("Test 1: Passed")
 
 if __name__ == '__main__':
      main()
