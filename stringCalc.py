6941abfad001208085700f9e74a64d28e54fe34d
JP5252 <justin.pearson52@hotmail.com>
2023-02-03 09:01:19 -0700

    add test 3

--- stringCalc.py
+++ stringCalc.py
@@ -14,11 +14,19 @@ def main():
 		print("Test 1: Failed")
 	elif (add(num1) == 0):
 		print("Test 1: Passed")
-	#test 1 take in null/empty string input
+
+	#test 2 take in string return int
 	num2 = "1"
 	if (add(num2) != 1):
 		print("Test 1: Failed")
 	elif (add(num2) == 1):
+		print("Test 1: Passed")
+	
+	#test 3 take in string with multiple numbers return added
+	num3 = "1,2"
+	if (add(num3) != 3):
+		print("Test 1: Failed")
+	elif (add(num3) == 3):
 		print("Test 1: Passed")
 
 if __name__ == '__main__':
